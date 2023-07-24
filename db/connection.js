e791a6d40412e26a30f62b36171248868dc7afb2
Jeremy Boyer <jeremytboyer@gmail.com>
2023-07-24 15:20:42 -0400

    ready for deploy

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('tech_blog_db', 'root', '', {
+const isProduction = process.env.PORT;
+let sequelize;
+
+if (isProduction) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    dialect: "mysql",
+  })} else {
+  sequelize = new Sequelize('tech_blog_db', 'root', '', {
   host: '127.0.0.1',
   dialect: 'mysql'
-});
+});}
 
 module.exports = sequelize;
